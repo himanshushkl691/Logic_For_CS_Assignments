c01971324d7c5a31b6b26ecf7e9de521a634d082
Himanshu Shukla <36098036+himanshushkl691@users.noreply.github.com>
2020-11-08 00:04:23 +0530

    10 done

--- Coq/ASSG2B/ASSG2B_10.v
+++ Coq/ASSG2B/ASSG2B_10.v
@@ -7,5 +7,11 @@ Section Pred_Logic_Assg2b.
     destruct H1 as [a H2].
     destruct H2 as [b H2].
     case H2 as [H2_i | H2_ii].
-    - exact H2_i.
-      - 
+    - exists a.
+      exists b.
+      exact H2_i.
+    - exists b.
+      exists a.
+      exact H2_ii.
+  Qed.
+  
