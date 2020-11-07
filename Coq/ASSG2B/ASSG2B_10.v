c549ef8d1b7ed9675ff32a4d160e66f94ec3fd32
Himanshu <himanshushkl691@gmail.com>
2020-11-07 22:54:52 +0530

    continue from 10 onwards

--- Coq/ASSG2B/ASSG2B_10.v
+++ Coq/ASSG2B/ASSG2B_10.v
@@ -1,3 +1,11 @@
 Section Pred_Logic_Assg2b.
   Variable V : Set.
-  
+  Variable S : V -> (V -> Prop).
+  Hypothesis H1 : exists x y : V, S x y \/ S y x.
+  Lemma ASSG2B_10 : exists x y : V, S x y.
+  Proof.
+    destruct H1 as [a H2].
+    destruct H2 as [b H2].
+    case H2 as [H2_i | H2_ii].
+    - exact H2_i.
+      - 
